74b4584a08461a4fab31112437e484c43d684d5b
arijhajji <61475226+arijhajji-1@users.noreply.github.com>
2023-07-26 17:25:51 +0100

    navbar tailwind

---
+++ Pages/index.tsx
@@ -0,0 +1,11 @@
+import Navigation from './Navigations';
+
+export default function HomePage() {
+  return (
+    <div>
+      <Navigation />
+      {/* Rest of the page content */}
+      
+    </div>
+  );
+}
